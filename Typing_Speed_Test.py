bf2b28ebb0001dd252b88d1aad69cff4891810ad
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-06 22:36:42 +0600

    calculate the typing mistake

--- Typing_Speed_Test.py
+++ Typing_Speed_Test.py
@@ -1,6 +1,19 @@
 from time import *
 import random as r
 
+def mistake(parTest,userInputTest): #calculate the typing mistack
+    error = 0
+    for i in range(len(parTest)):
+        try:
+            if parTest[i] != userInputTest[i]:
+                error = error + 1
+        except:
+            error = error + 1
+    return error
+
+
+
+
 
 test = ["A paragragraph is a self-contained unit of discourse in write","I am Ahmad Shawun", "welcome the python"] 
 
@@ -9,3 +22,5 @@ print('      ****** typing speed ******** ')
 print(test1)
 print() # breaking for 1 line
 print() # brack for 2 lines
+
+testInput = input(" Enter: ")
