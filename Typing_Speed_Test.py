b2b2d2c9055e723f92775f4c81dea47c867ef85b
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-06 22:58:17 +0600

    project done

--- Typing_Speed_Test.py
+++ Typing_Speed_Test.py
@@ -19,19 +19,27 @@ def speedTime(timeStart, timeEnd, userInput): # speed time
     timeRoundOff = round(timeDelay, 2) # 2digit
     speed = len(userInput) / timeRoundOff
     return round(speed) #
+if __name__ == '__main__':
+    while True:
+        ck = input( " Ready to test : yes / no : ")
+        if ck == "yes":
+            test = ["A paragragraph is a self-contained unit of discourse in write","I am Ahmad Shawun", "welcome the python"] 
 
+            test1 = r.choice(test) #randomly show the test set result
+            print('      ****** typing speed ******** ')
+            print(test1)
+            print() # breaking for 1 line
+            print() # brack for 2 lines
 
-test = ["A paragragraph is a self-contained unit of discourse in write","I am Ahmad Shawun", "welcome the python"] 
+            time1 = time() # fristly take the time when it started
+            testInput = input(" Enter: ")
+            time2 = time() # end the time when user done
 
-test1 = r.choice(test) #randomly show the test set result
-print('      ****** typing speed ******** ')
-print(test1)
-print() # breaking for 1 line
-print() # brack for 2 lines
-
-time1 = time() # fristly take the time when it started
-testInput = input(" Enter: ")
-time2 = time() # end the time when user done
-
-print(' Speed : ',speedTime(time1, time2, testInput), 'word/second')
-print(' Error : ', mistake(test1, testInput))
+            print(' Speed : ',speedTime(time1, time2, testInput), 'word/second')
+            print(' Error : ', mistake(test1, testInput))
+        elif ck == "no":
+            print(" Thank you for visiting this")
+            break
+        
+        else:
+            print(" Wrong input")
