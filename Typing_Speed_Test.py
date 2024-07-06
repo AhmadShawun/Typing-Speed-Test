8fa7f0c9ca65d77ddeb4b2f77782abf2d4207822
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-06 22:27:56 +0600

    randomly show the set output

--- Typing_Speed_Test.py
+++ Typing_Speed_Test.py
@@ -1,1 +1,11 @@
-print('typing speed test')
+from time import *
+import random as r
+
+
+test = ["A paragragraph is a self-contained unit of discourse in write","I am Ahmad Shawun", "welcome the python"] 
+
+test1 = r.choice(test) #randomly show the test set result
+print('      ****** typing speed ******** ')
+print(test1)
+print() # breaking for 1 line
+print() # brack for 2 lines
